1d228657ec9a3359eed2ac436e3794e32e5695c1
Dave <davepenfold@gmail.com>
2021-05-29 23:41:50 -0700

    Remove framer-motion from components and project

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 const Message = ({ children }) => (
-  <motion.div
-    transition={{ duration: .5 }}
-    initial={{ y: 50, opacity: 0 } }
-    animate={{ y: 0, opacity: 1 }}
-  >
+  <div>
     {children}
-  </motion.div>
+  </div>
 );
 
 export default Message;
